6c461ed79d8ed30fa2254223b6d4006410967361
dzadoroz <dandriq@icloud.com>
2025-05-15 15:59:28 +0300

    Task 9, update with requested changes

--- ecentaamerica-partner/ECENTAAMERICA_PARTNER/GlobalScripts/dz_module_for_calc.py
+++ ecentaamerica-partner/ECENTAAMERICA_PARTNER/GlobalScripts/dz_module_for_calc.py
@@ -4,6 +4,7 @@ def count_ext(item, cf):
         Formula: quantity * methodPrice
         param IQuoteItem item: item from quote Items collection
         param string cf: needed custom field name as string
+		return: float
     '''
     quantity = item.Quantity
     customField = item.GetCustomField(cf)
@@ -11,32 +12,30 @@ def count_ext(item, cf):
         return quantity * customField.Value
 
 
-def copyDown(context, cf, prices_dict):
+def copyDown(quote_items, cf):
     '''
-        Copies % of MSRP from Main Item to it's discountable line items
+        Copies % of MSRP from Main Item to its discountable line items
         param Quote2Context context: context for current quote
         param string cf: needed custom field name as string
-		param dict prices_dict: key - Part_number, values - object from sql table row
+        return: void
     '''
-    quote_items = context.Quote.GetAllItems()
     main_item = None
     for item in quote_items:
-        part_num = item.PartNumber
-        if part_num not in prices_dict:
+        if item.IsLineItem:
             continue
-        if not item.IsLineItem:
-            main_item = item
-            continue
-        if main_item and item.ParentItemId == main_item.Id and prices_dict[part_num].Discountable:
-            item[cf] = main_item[cf]
+        main_item = item.AsMainItem
+        for child in main_item.GetChildItems():
+            if not child["DZ_Discountable"]:
+                continue
+            child[cf] = main_item[cf]
 
 
-def get_DZ_CUSTOM_PRICE_LIST(context):
+def get_DZ_CUSTOM_PRICE_LIST(quote_items):
     '''
-        Gets a list from DZ_CUSTOM_PRICE_LIST custom table, and makes it a dict with Part_number as key, 		 and sql table row as object
+        Gets a list from DZ_CUSTOM_PRICE_LIST custom table, and makes it a dict with Part_number as key, and sql table row as object
         param Quote2Context context: context for current quote
+		return: dict
     '''
-    quote_items = context.Quote.GetAllItems()
     part_num_list = [item.PartNumber for item in quote_items]
     if not part_num_list:
         return {}
--- ecentaamerica-partner/ECENTAAMERICA_PARTNER/GlobalScripts/dz_read_prices_2.py
+++ ecentaamerica-partner/ECENTAAMERICA_PARTNER/GlobalScripts/dz_read_prices_2.py
@@ -2,23 +2,24 @@
 from dz_module_for_calc import get_DZ_CUSTOM_PRICE_LIST
 
 target_part_num = "LP-B-500GB-24GB-M3-16i"
-if User.UserType.Name == "DZ_userType":
-    quote_items = context.Quote.GetAllItems()
-    main_item = next((item for item in quote_items if item.PartNumber == target_part_num), None)
-    if not main_item:
-        exit()
-    main_item_proc_msrp = main_item["DZ_MSRP"]
-    prices_dict = get_DZ_CUSTOM_PRICE_LIST(context)
-    for item in quote_items:
-        if item.Id != main_item.Id and item.ParentItemId != main_item.Id:
-            continue
-        part_num = item.PartNumber
-        if part_num not in prices_dict:
-            continue
-        price_data = prices_dict[part_num]
-        item["DZ_MSRP_2"] = price_data.MSRP
-        item["DZ_DNET_2"] = price_data.DNET
-        if not price_data.Discountable:
-            item["DZ_MSRP"] = 0
-            continue
-        item["DZ_MSRP"] = main_item_proc_msrp
+if not User.UserType.Name == "DZ_userType":
+    exit()
+quote_items = context.Quote.GetAllItems()
+main_item = next((item for item in quote_items if item.PartNumber == target_part_num), None)
+if not main_item:
+    exit()
+main_item_proc_msrp = main_item["DZ_MSRP"] or 0
+prices_dict = get_DZ_CUSTOM_PRICE_LIST(quote_items)
+for item in quote_items:
+    if item.Id != main_item.Id and item.ParentItemId != main_item.Id:
+        continue
+    part_num = item.PartNumber
+    if part_num not in prices_dict:
+        continue
+    price_data = prices_dict[part_num]
+    item["DZ_MSRP_2"] = price_data.MSRP
+    item["DZ_DNET_2"] = price_data.DNET
+    item["DZ_Discountable"] = price_data.Discountable
+    if not item["DZ_Discountable"]:
+        continue
+    item["DZ_MSRP"] = main_item_proc_msrp
